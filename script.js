a05161a090d90f7097684835747426df71abcfef
kandalaza <ashabbakaev73@gmail.com>
2025-12-04 17:44:58 +0300

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const concatenateStrings = (a, b) => (a + b).replace(/ /g, '');
+console.log(concatenateStrings('aa', 'bb')); 
+console.log(concatenateStrings('aa', ' '));
+console.log(concatenateStrings('', 'bb'));
+
+
+const getStringLength = str => str.length;
+console.log(getStringLength('abc'));
+console.log(getStringLength(''));
+
+
+const getStringFromTemplate = (firstName, lastName) => `Hello, ${firstName} ${lastName}!`;
+console.log(getStringFromTemplate('John', 'Doe'));
+console.log(getStringFromTemplate('Chuck', 'Norris'));
+
+
+const getChar = (str, index) => str[index - 1];
+console.log(getChar('John Doe', 1));
+console.log(getChar('cat', 3));
+
+
+const removeFirstOccurrences = (str, substr) => str.replace(substr, '');
+console.log(removeFirstOccurrences('To be or not to be' , 'not'));
+console.log(removeFirstOccurrences('I like legends', 'end'));
+console.log(removeFirstOccurrences('ABABAB' , 'BA'));              
+
+
+const getRectangleString = (w, h) =>
+    (w < 2 || h < 2) 
+        ? '*'.repeat(w) 
+        : '*'.repeat(w) + '\n' 
+        + ('*' + ' '.repeat(w - 2) + '*\n').repeat(h - 2) 
+        + '*'.repeat(w);
+
+console.log(getRectangleString(5, 4));
